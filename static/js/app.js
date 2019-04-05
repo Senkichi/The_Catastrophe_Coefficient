80e56aba80fa3fc669370820efca9bf53441c156
chdacamp <43750486+chdacamp@users.noreply.github.com>
2019-04-04 22:12:35 -0700

    working heat map

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,11 @@
 
+var	year = d3.select("#year").node().value;
+var	model = d3.select("#model").node().value;
+var	algorithm = d3.select("#algorithm").node().value;
 
 var url = "./static/data/dummy_data.json"
 
+//var url = "api/v1.0/{year}/{model}/{algorithm}"
 
 var baseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -11,13 +15,12 @@ var baseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
 });
 
 
-var	yearSelect = d3.select("#year").node().value;
-var	typeSelect = d3.select("#dataType").node().value;
+
 
 d3.json(url, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
 	console.log(data.length)
-	createFeatures(data);
+	createFeatures({max: 200,data});
 })
 
 function createFeatures(sourceData) {
@@ -27,14 +30,14 @@ function createFeatures(sourceData) {
 var cfg = {
   // radius should be small ONLY if scaleRadius is true (or small radius is intended)
   // if scaleRadius is false it will be the constant radius used in pixels
-  "radius": 2,
+  "radius": 0.1,
   "maxOpacity": .8, 
   // scales the radius based on map zoom
   "scaleRadius": true, 
   // if set to false the heatmap uses the global maximum for colorization
   // if activated: uses the data maximum within the current map boundaries 
   //   (there will always be a red spot with useLocalExtremas true)
-  "useLocalExtrema": true,
+  "useLocalExtrema": false,
   // which field name in your data represents the latitude - default "lat"
   latField: 'lat',
   // which field name in your data represents the longitude - default "lng"
