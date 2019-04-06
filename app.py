acfe044131114c4a8741c9b6403ded8e614e6af8
Sandeep Palavalasa <sandeep.palavalasa@gmail.com>
2019-04-05 19:28:24 -0700

    updated API

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ app = Flask(__name__,template_folder='static/templates')
 #allow cross origin script access so can access via js
 CORS(app)
 
-mongo = PyMongo(app, uri="mongodb://root:ucbmongodb@35.184.4.63:27017/ACS")
+mongo = PyMongo(app, uri="mongodb://root:ucbmongodb@35.184.4.63:27017/weather")
 conn = 'mongodb://root:ucbmongodb@35.184.4.63:27017'
 client = pymongo.MongoClient(conn)
-db = client.ACS
+db = client.weather
 
 
 # Set up routes
@@ -26,34 +26,14 @@ def index():
     return render_template("index.html")
 
 # Set up api routes
-@app.route("/api/v1/census")
-def censusAllYears():
-    # print(db.census_by_county.find())
-    censusdata = dumps(db.census_by_county.find({},{'Unnamed: 0': 0, '_id': 0}))
-    # print(censusdata)
-    return jsonify(json.loads(censusdata))
+@app.route("/api/v1/<year>/<severity>/<algorithm>")
+def firePrediction(year,severity,algorithm):
+    print("reached the API")
+    collection_name = severity + '_' +algorithm + '_' + year
+    predictionData = dumps(db[collection_name].find({},{'_id': 0,'x':1,'y':1,'prediction':1}))
+    print(predictionData)
+    return jsonify(json.loads(predictionData))
 
-
-@app.route("/api/v1/census/year/<year>")
-def censusByYear(year):
-    result = dumps(db.census_by_county.find({},{
-        year+'IN':1, 
-        year+'OUT':1, 
-        year+'NET':1,
-        year+'RENT':1,
-        year+'MED_INC':1,
-        'County': 1,
-        'Lat': 1,
-        'Lon': 1,
-        'NAME': 1, 
-        '_id':0
-    }))
-    return jsonify(json.loads(result))
-
-@app.route("/api/v1/choropleth")
-def choro_dict():
-    result = dumps(db.choropleth_master.find({}))
-    return jsonify(json.loads(result))
 
 if __name__ == '__main__':
     app.run(debug=True)
