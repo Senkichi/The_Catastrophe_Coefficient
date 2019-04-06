0c168e9516efd7a0eeef587e9e649f82c5edd1ea
chdacamp <43750486+chdacamp@users.noreply.github.com>
2019-04-06 00:12:37 -0700

    fully working map!

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,14 @@
 var	year = d3.select("#year").node().value;
 var	model = d3.select("#model").node().value;
 var	algorithm = d3.select("#algorithm").node().value;
+var max = 3
+var min = 0
+var heatmapLayer
+//var url = "./static/data/dummy_data.json"
 
-var url = "./static/data/dummy_data.json"
+var url = `./api/v1/${year}/${model}/${algorithm}`
+//var url = './api/v1/2050/harsh/cnnsequential'
 
-//var url = `api/v1.0/${year}/${model}/${algorithm}`
 
 var baseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -14,12 +18,16 @@ var baseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
   accessToken: API_KEY
 });
 
+var map = L.map("map", {
+    center: [36.7783, -119.4179],
+    zoom: 6,
+	layers: [baseLayer]
+  });
 
-
-
+//Initial Draw
 d3.json(url, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
-	createFeatures({max: 200,min: 0,data});
+	createFeatures({max: max,min: min,data});
 })
 
 function createFeatures(sourceData) {
@@ -29,7 +37,7 @@ function createFeatures(sourceData) {
 var cfg = {
   // radius should be small ONLY if scaleRadius is true (or small radius is intended)
   // if scaleRadius is false it will be the constant radius used in pixels
-  "radius": 0.1,
+  "radius": 0.3,
   "maxOpacity": .8, 
   // scales the radius based on map zoom
   "scaleRadius": true, 
@@ -38,22 +46,16 @@ var cfg = {
   //   (there will always be a red spot with useLocalExtremas true)
   "useLocalExtrema": false,
   // which field name in your data represents the latitude - default "lat"
-  latField: 'lat',
+  latField: 'y',
   // which field name in your data represents the longitude - default "lng"
-  lngField: 'lng',
+  lngField: 'x',
   // which field name in your data represents the data value - default "value"
-  valueField: 'Prediction'
+  valueField: 'prediction'
 };
 
-var heatmapLayer = new HeatmapOverlay(cfg);
+heatmapLayer = new HeatmapOverlay(cfg);
 
-var map = L.map("map", {
-    center: [36.7783, -119.4179],
-    zoom: 6,
-	layers: [baseLayer, heatmapLayer]
-  });
-
-
+map.addLayer(heatmapLayer)
 /*   var Legend = L.Control.extend({  
   options: {
     position: 'bottomright'
@@ -74,4 +76,17 @@ var map = L.map("map", {
 heatmapLayer.setData(sourceData);
 };
 
+//REDRAW after button click
 
+document.getElementById("redraw").onclick = function() {
+
+	year = d3.select("#year").node().value;
+	model = d3.select("#model").node().value;
+	algorithm = d3.select("#algorithm").node().value;
+	url = `./api/v1/${year}/${model}/${algorithm}`
+	map.removeLayer(heatmapLayer);	
+	d3.json(url, function(data) {
+	createFeatures({max: max,min: min,data});
+	console.log('redraw')
+})
+}
